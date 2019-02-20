1b47a05a619fb457beeeb889f86525d98e5dd08d
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-02-20 09:48:17 +0200

    refactor ecgpuwave code

--- pyhrv/wfdb/qrs.py
+++ pyhrv/wfdb/qrs.py
@@ -12,150 +12,160 @@ import pyhrv.wfdb.utils as utils
 from pyhrv.wfdb.consts import ECGPUWAVE_BIN
 
 
-def ecgpuwave_detect(sig=None, fs=None, rec_path=None, channel=None,
-                     sampfrom=0, sampto=-1, peaks=True, **kw):
+def ecgpuwave_detect_sig(sig, fs, from_samp=0, to_samp=-1, **kw):
     """
-    Detect QRS using the ecgpuwave detector.
-    Can work with either a PhysioNet record name or data from a single channel
-    :param sig: A 1-d numpy array containing one ECG channel. Can't be
-    combined with rec_path.
-    :param fs: The sampling frequency of that channel. Can't be
-    combined with rec_path.
-    :param rec_path: Path to PhysioNet record. If specified, sig and fs must
-    be None.
-    :param channel: Index of channel to read when using rec_path. If None,
-    it will be heuristically estimated.
-    :param sampfrom: Start sample index.
-    :param sampto: End sample index. -1 for end of record.
-    :param peaks: Whether to detect R-peaks (True) or QRS onset (False).
+    Runs the ecgpuwave QRS detector on a single channel ECG signal, and returns
+    the indices of detections.
+
+    :param sig: A 1-d numpy array containing one ECG channel.
+    :param fs: The sampling frequency of that channel.
+    :param from_samp: Start sample index.
+    :param to_samp: End sample index. -1 for end of record.
     :return: A numpy array of sample indices corresponding to detected
     features (either R-peaks or QRS onset).
     """
+    assert sig.ndim == 1
+    sig = sig[from_samp:to_samp].reshape(-1, 1)
 
-    if not ((sig is not None and fs is not None) or rec_path is not None):
-        raise ValueError("Must set either sig and fs or rec_path, not both "
-                         "or none")
+    # We'll write the given data to a temporary record
+    temp_dir = tempfile.gettempdir()
+    temp_recname = f'tmp_{random.randint(0, 10000):04d}'
+    rec_path = os.path.join(temp_dir, temp_recname)
 
-    from_time = f's{sampfrom:d}'
-    to_time = 'e' if sampto == -1 else f's{sampto:d}'
-
-    # Write the signal data to a WFDB record
-    delete_rec = False
-    if not rec_path:
-        assert sig.ndim == 1
-        temp_dir = tempfile.gettempdir()
-        temp_recname = f'tmp_{random.randint(0, 10000):04d}'
-        sig = sig[sampfrom:sampto].reshape(-1, 1)
+    try:
         wfdb.wrsamp(record_name=temp_recname, fs=fs, units=['mV'],
                     sig_name=['ECG'], p_signal=sig,
                     write_dir=temp_dir, fmt=['212'])
-        rec_path = os.path.join(temp_dir, temp_recname)
-        delete_rec = True
-    else:
-        if channel is None:
-            channel = utils.find_ecg_channel(rec_path)
 
-    # Apply peak detector
+        return ecgpuwave_detect_rec(rec_path, channel=0, **kw)
+    finally:
+        # Remove the temporary record
+        for f in glob.glob(os.path.join(rec_path, '.*')):
+            os.remove(f)
+
+
+def ecgpuwave_detect_rec(rec_path, channel=None, from_time=None, to_time=None,
+                         **kw):
+    """
+    Runs the ecgpuwave QRS detector on a single channel ECG signal from a
+    given PhysioNet record, and returns the indices of detections.
+
+    :param rec_path: Path to PhysioNet record without extension.
+    :param channel: Index of channel to read when using rec_path. If None,
+    it will be heuristically estimated.
+    :param from_time: Start time. A string in PhysioNet time format [1]_.
+    :param to_time: End time.  A string in PhysioNet time format [1]_.
+    :return: A numpy array of sample indices corresponding to detected
+    features (either R-peaks or QRS onset).
+
+    .. [1] https://www.physionet.org/physiotools/wag/intro.htm#time
+    """
+
+    if channel is None:
+        channel = utils.find_ecg_channel(rec_path)
+
+    ann_ext = 'ecgatr'
     try:
-        detector = ECGPuWave()
-        ann_ext = 'ecgatr'
-        detector(rec_path, ann_ext, channel=channel,
-                 from_time=from_time, to_time=to_time)
-        ann_type = 'N' if peaks else '('
+        # Run ecgpuwave
+        ecgpuwave_wrapper(rec_path, ann_ext, channel=channel,
+                          from_time=from_time, to_time=to_time)
+
+        # Read the annotations from the file it created.
+        ann_type = 'N'
         ann_to_idx = utils.rdann_by_type(rec_path, ann_ext, types=ann_type)
         return ann_to_idx[ann_type]
     finally:
-        if delete_rec:
-            for f in glob.glob(os.path.join(rec_path, '.*')):
-                os.remove(f)
+        # Remove the annotation file
+        ann_file = os.path.join(rec_path, f'.{ann_ext}')
+        if os.path.exists(ann_file):
+            os.remove(ann_file)
 
 
-class ECGPuWave(object):
+def ecgpuwave_wrapper(record: str, out_ann_ext: str,
+                      in_ann_ext: str = None, channel: int = None,
+                      from_time: str = None, to_time: str = None):
     """
     A wrapper for PhysioNet's ecgpuwave [1]_ tool, which segments ECG beats.
 
+    Runs the ecgpuwave tool on a given record, producing an annotation
+    file with a specified extension.
+
+    :param record: Path to PhysioNet record, e.g. foo/bar/123 (no file
+        extension allowed).
+    :param out_ann_ext: The extension of the annotation file to create.
+    :param in_ann_ext: Read an annotation file with the given extension
+        as input to specify beat types in the record.
+    :param channel: The index of the channel in the record to
+        analyze.
+    :param from_time: Start at the given time. Should be a string in one of
+        the PhysioNet time formats (see [2]_).
+    :param to_time: Stop at the given time. Should be a string in one of
+        the PhysioNet time formats (see [2]_).
+    :return: True if ran without error.
+
     .. [1] https://www.physionet.org/physiotools/ecgpuwave/
+       [2] https://www.physionet.org/physiotools/wag/intro.htm#time
     """
 
-    def __init__(self, ecgpuwave_bin=ECGPUWAVE_BIN):
-        self.ecgpuwave_bin = ecgpuwave_bin
+    rec_dir = os.path.dirname(record)
+    rec_name = os.path.basename(record)
+    ecgpuwave_rel_path = os.path.abspath(ECGPUWAVE_BIN)
 
-    def __call__(self, record: str, out_ann_ext: str,
-                 in_ann_ext: str = None, channel: int = None,
-                 from_time: str = None, to_time: str = None):
-        """
-        Runs the ecgpuwave tool on a given record, producing an annotation
-        file with a specified extension.
+    ecgpuwave_command = [
+        ecgpuwave_rel_path,
+        '-r', rec_name,
+        '-a', out_ann_ext,
+    ]
 
-        :param record: Path to PhysioNet record, e.g. foo/bar/123 (no file
-            extension allowed).
-        :param out_ann_ext: The extension of the annotation file to create.
-        :param in_ann_ext: Read an annotation file with the given extension
-            as input to specify beat types in the record.
-        :param channel: The index of the channel in the record to
-            analyze.
-        :param from_time: Start at the given time. Should be a string in one of
-            the PhysioNet time formats (see [2]_).
-        :param to_time: Stop at the given time. Should be a string in one of
-            the PhysioNet time formats (see [2]_).
-        :return: True if ran without error.
+    if in_ann_ext:
+        ecgpuwave_command += ['-i', in_ann_ext]
 
-        .. [2] https://www.physionet.org/physiotools/wag/intro.htm#time
-        """
+    if channel:
+        ecgpuwave_command += ['-s', str(channel)]
 
-        rec_dir = os.path.dirname(record)
-        rec_name = os.path.basename(record)
-        ecgpuwave_rel_path = os.path.abspath(self.ecgpuwave_bin)
+    if from_time:
+        ecgpuwave_command += ['-f', from_time]
 
-        ecgpuwave_command = [
-            ecgpuwave_rel_path,
-            '-r', rec_name,
-            '-a', out_ann_ext,
-        ]
+    if to_time:
+        ecgpuwave_command += ['-t', to_time]
 
-        if in_ann_ext:
-            ecgpuwave_command += ['-i', in_ann_ext]
+    try:
+        ecgpuwave_result = subprocess.run(
+            ecgpuwave_command,
+            check=True, shell=False, universal_newlines=True, timeout=10,
+            cwd=rec_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
 
-        if channel:
-            ecgpuwave_command += ['-s', str(channel)]
+        # ecgpuwave can sometimes fail but still return 0, so need to
+        # also check the stderr output.
+        if ecgpuwave_result.stderr:
+            # Annoying case: sometimes ecgpuwave writes to stderr but it's
+            # not an error...
+            if not re.match(r'Rearranging annotations[\w\s.]+done!',
+                            ecgpuwave_result.stderr):
+                raise subprocess.CalledProcessError(0, ecgpuwave_command)
 
-        if from_time:
-            ecgpuwave_command += ['-f', from_time]
+    except subprocess.CalledProcessError as process_err:
+        warnings.warn(f'Failed to run ecgpuwave on record '
+                      f'{record}:\n'
+                      f'stderr: {ecgpuwave_result.stderr}\n'
+                      f'stdout: {ecgpuwave_result.stdout}\n')
+        return False
 
-        if to_time:
-            ecgpuwave_command += ['-t', to_time]
-
-        try:
-            ecgpuwave_result = subprocess.run(
-                ecgpuwave_command,
-                check=True, shell=False, universal_newlines=True, timeout=10,
-                cwd=rec_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-            )
-
-            # ecgpuwave can sometimes fail but still return 0, so need to
-            # also check the stderr output.
-            if ecgpuwave_result.stderr:
-                # Annoying case: sometimes ecgpuwave writes to stderr but it's
-                # not an error...
-                if not re.match(r'Rearranging annotations[\w\s.]+done!',
-                                ecgpuwave_result.stderr):
-                    raise subprocess.CalledProcessError(0, ecgpuwave_command)
-
-        except subprocess.CalledProcessError as process_err:
-            warnings.warn(f'Failed to run ecgpuwave on record '
-                          f'{record}:\n'
-                          f'stderr: {ecgpuwave_result.stderr}\n'
-                          f'stdout: {ecgpuwave_result.stdout}\n')
-            return False
-
-        except subprocess.TimeoutExpired as timeout_err:
-            warnings.warn(f'Timed-out runnning ecgpuwave on record '
-                          f'{record}: '
-                          f'{ecgpuwave_result.stdout}')
-            return False
-        finally:
-            # Remove tmp files created by ecgpuwave
-            for tmpfile in glob.glob(f'{rec_dir}/fort.*'):
+    except subprocess.TimeoutExpired as timeout_err:
+        warnings.warn(f'Timed-out runnning ecgpuwave on record '
+                      f'{record}: '
+                      f'{ecgpuwave_result.stdout}')
+        return False
+    finally:
+        # Remove tmp files created by ecgpuwave
+        for tmpfile in glob.glob(f'{rec_dir}/fort.*'):
+            try:
                 os.remove(tmpfile)
+            except FileNotFoundError:
+                # When running multiple ecgpuwave processes in parallel, it's
+                # possible file was already deleted by another process
+                pass
 
-        return True
+    return True
+
--- tests/wfdb/test_ecgpuwave.py
+++ tests/wfdb/test_ecgpuwave.py
@@ -5,16 +5,15 @@ import warnings
 from pathlib import Path
 import wfdb
 
-from pyhrv.wfdb.qrs import ECGPuWave
+from pyhrv.wfdb.qrs import ecgpuwave_wrapper
 import tests.wfdb
 
 RESOURCES_PATH = f'{tests.wfdb.TEST_RESOURCES_PATH}/ecgpuwave'
 TEST_ANN_EXT = 'test'
 
 
-class ECGPuWaveTest(unittest.TestCase):
+class ECGPuWaveWrapperTest(unittest.TestCase):
     def setUp(self):
-        self.ecgpuwave = ECGPuWave()
         self.test_rec = f'{RESOURCES_PATH}/100s'
         self.test_rec_ann_ext = 'atr'
 
@@ -30,40 +29,40 @@ class ECGPuWaveTest(unittest.TestCase):
 
     def test_sig0_full_noatr_num_annotations(self):
         for signal_idx in [0, None]:
-            res = self.ecgpuwave(self.test_rec, TEST_ANN_EXT,
-                                 channel=signal_idx)
+            res = ecgpuwave_wrapper(self.test_rec, TEST_ANN_EXT,
+                                    channel=signal_idx)
             self.assertTrue(res)
             self._helper_check_num_annotations(684)
 
     def test_sig1_full_noatr_num_annotations(self):
-        res = self.ecgpuwave(self.test_rec, TEST_ANN_EXT, channel=1)
+        res = ecgpuwave_wrapper(self.test_rec, TEST_ANN_EXT, channel=1)
         self.assertTrue(res)
         self._helper_check_num_annotations(655)
 
     def test_sig0_first30s_noatr_num_annotations(self):
         for to_time in ['0:30', '0:0:30', '00:00:30', 's10800']:
-            res = self.ecgpuwave(self.test_rec, TEST_ANN_EXT,
-                                 to_time=to_time)
+            res = ecgpuwave_wrapper(self.test_rec, TEST_ANN_EXT,
+                                    to_time=to_time)
             self.assertTrue(res)
             self._helper_check_num_annotations(349)
 
     def test_sig1_last20s_noatr_num_annotations(self):
         for from_time in ['0:40', '0:0:40', '00:00:40', 's14400']:
-            res = self.ecgpuwave(self.test_rec, TEST_ANN_EXT, channel=1,
-                                 from_time=from_time)
+            res = ecgpuwave_wrapper(self.test_rec, TEST_ANN_EXT, channel=1,
+                                    from_time=from_time)
             self.assertTrue(res)
             self._helper_check_num_annotations(209)
 
     def test_sig0_full_withatr_num_annotations(self):
-        res = self.ecgpuwave(self.test_rec, TEST_ANN_EXT,
-                             in_ann_ext=self.test_rec_ann_ext)
+        res = ecgpuwave_wrapper(self.test_rec, TEST_ANN_EXT,
+                                in_ann_ext=self.test_rec_ann_ext)
         self.assertTrue(res)
         self._helper_check_num_annotations(670)
 
     def test_invalid_record_should_fail(self):
         try:
             with warnings.catch_warnings(record=True) as w:
-                res = self.ecgpuwave(f'{RESOURCES_PATH}/foo', 'bar')
+                res = ecgpuwave_wrapper(f'{RESOURCES_PATH}/foo', 'bar')
                 self.assertFalse(res)
                 self.assertEqual(1, len(w))
         finally:
