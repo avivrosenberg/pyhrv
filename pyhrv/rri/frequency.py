827c85c480b7ebdf2a61296cc7053d4c2de32864
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-07-30 15:29:27 +0300

    frequency: implement lomb periodogram for intervals

---
+++ pyhrv/rri/frequency.py
@@ -0,0 +1,117 @@
+import math
+import logging
+from typing import Callable
+
+import numpy as np
+import scipy.signal as sps
+
+from pyhrv import utils
+
+logger = logging.getLogger(__name__)
+
+
+def pxx_lomb(rri: np.ndarray, f_axis: np.ndarray, trr: np.ndarray = None,
+             t_win: float = None, win_func: Callable = sps.windows.hamming):
+    """
+    Lomb-Scargle periodogram of RR intervals.
+    Can optionally split the data into equal-length windows and average
+    the periodogram from each window.
+
+    :param rri: RR intervals.
+    :param f_axis: Frequencies, in Hz, to evaluate at.
+    :param trr: RR intervals times.
+    :param t_win: Duration in seconds of each window. If none, no windowing.
+    :param win_func: Window function to apply to the data (e.g. Hamming).
+    None to disable (use rectangular window).
+    :return: Periodogram.
+    """
+    # Standardize input vectors
+    rri, trr = utils.standardize_rri_trr(rri, trr)
+
+    if not t_win:
+        t_win = math.floor(trr[-1] - trr[0])
+
+    if not win_func:
+        win_func = sps.windows.boxcar
+
+    # Convert frequency to normalized (angular freq)
+    w_axis = f_axis * 2 * math.pi
+
+    sig_duration = trr[-1]
+    window_starts = (t_win * i for i in range(2 ** 63 - 1))
+
+    pxx = np.zeros_like(f_axis)
+    win_idx = 0
+    for win_idx, win_start in enumerate(window_starts):
+        win_end = win_start + t_win
+        if win_end > sig_duration:
+            break
+
+        win_idx = (trr >= win_start) & (trr < win_end)
+        rri_win = rri[win_idx]
+        trr_win = trr[win_idx]
+
+        min_samples_nyq = math.ceil(2 * f_axis[-1] * t_win)
+        if len(rri_win) < min_samples_nyq:
+            logger.warning(f'Nyquist criterion not met for lomb periodogram '
+                           f'in window {win_idx} '
+                           f'({len(rri_win)}/{min_samples_nyq} samples). '
+                           f'Skipping.')
+            continue
+
+        # Apply window
+        win_coeffs = win_func(len(rri_win))
+        rri_win *= win_coeffs
+
+        # Calculate periodogram
+        pxx_win = sps.lombscargle(trr, rri, w_axis,
+                                  precenter=True, normalize=False, )
+        # Window gain correction
+        pxx_win *= 1 / np.mean(win_coeffs)
+        pxx += pxx_win
+
+    pxx /= win_idx
+    return pxx
+
+
+def build_uniform_freq_axis(t_win: float, f_min: float, f_max: float,
+                            resample_factor: float = 2,
+                            oversample_factor: float = 1):
+    """
+    Builds a frequency axis for an RR interval signal.
+    Since RR intervals are by nature non-uniformly sampled, we assume that
+    they will be resampled at least at twice the maximal frequency we wish
+    to resolve prior to spectral analysis.
+
+    Also, we handle the case where the signal will be split into windows of
+    length t_win seconds prior to spectral analysis.
+
+    :param t_win: Duration in seconds of the signal.
+    :param f_min: Minimal frequency we wish to resolve.
+    :param f_max: Maximal frequency we wish to resolve.
+    :param resample_factor: Factor of f_max that the signal should be
+    resampled at (at least 2 to maintain Nyquist criterion).
+    :param oversample_factor: Factor to increase the frequency resolution of
+    the created frequency axis.
+    :return: (f_axis, fs_uni) a tuple of the frequency axis and the uniform
+    resampling frequency.
+    """
+
+    t_win_min = 1 / f_min  # minimal window to resolve f_min
+    t_win = max(t_win, t_win_min)
+
+    # Uniform sampling freq: Take at least 2x more than f_max
+    if resample_factor < 2:
+        raise ValueError("resample_factor must be at least 2")
+    fs_uni = resample_factor * f_max  # Hz
+
+    # Num samples per window
+    n_win_uni = math.floor(t_win / (1 / fs_uni))
+
+    # Frequency axis
+    ts = t_win / (n_win_uni - 1)  # sampling interval
+    f_res = 1 / (n_win_uni * ts)  # freq resolution, aka f_min or delta_f
+    f_res /= oversample_factor  # Oversampling factor -> interpolation in freq
+    f_axis = np.r_[f_res:f_max:f_res]
+
+    return f_axis, fs_uni
---
+++ tests/rri/test_frequency.py
@@ -0,0 +1,66 @@
+import math
+
+import scipy.signal as sps
+import matplotlib.pyplot as plt
+
+import pytest
+import pyhrv.wfdb.rri
+import pyhrv.rri.processing
+import pyhrv.rri.frequency as frequency
+
+from ..wfdb import TEST_RESOURCES_PATH
+
+WFDB_TEST_RESOURCES_PATH = TEST_RESOURCES_PATH.joinpath('wfdb')
+
+
+class TestBuildUniformFrequencyAxis(object):
+    def test_1(self):
+        f_min, f_max = 0.003, 0.5
+        t_win = 200  # too low for f_min
+        resample_factor = 2.25
+        osf = 4
+
+        f_axis, fs = frequency.build_uniform_freq_axis(
+            t_win, f_min, f_max,
+            resample_factor=resample_factor, oversample_factor=osf)
+
+        assert fs == f_max * resample_factor
+
+        t_win = 1 / f_min
+        n_win = math.floor(t_win / (1 / fs))
+        f_res = (n_win - 1) / (n_win * t_win)  # freq resolution
+        f_res /= osf  # Oversampling factor -> interpolation in freq
+
+        assert f_axis[0] == pytest.approx(f_res, rel=1e-9)
+        assert f_axis[-1] == pytest.approx(
+            f_res + math.floor((f_max - f_res) / f_res) * f_res, rel=1e-9)
+
+
+class TestPxxLomb(object):
+    @classmethod
+    def setup_class(cls):
+        rec_path = WFDB_TEST_RESOURCES_PATH.joinpath('100')
+        trr, rri = pyhrv.wfdb.rri.ecgrr(rec_path, ann_ext='atr')
+        
+        cls.trr, cls.rri = pyhrv.rri.processing.filtrr(trr, rri)
+
+    def test_1(self):
+        t_win = 330
+        f_min, f_max = 1 / t_win, 0.4
+        osf = 4
+
+        f_axis, _ = frequency.build_uniform_freq_axis(t_win, f_min, f_max,
+                                                      oversample_factor=osf)
+        
+        i = 1
+        win_idx = (self.trr >= t_win*i) & (self.trr < t_win*(i+1))
+        rri_win = self.rri[win_idx]
+
+        pxx_lomb = frequency.pxx_lomb(rri_win, f_axis=f_axis)
+        # plt.plot(f_axis, pxx_lomb)
+        # plt.show()
+        
+        # Check location of highest peak
+        peaks_idx, _ = sps.find_peaks(pxx_lomb, height=0.1)
+        assert f_axis[peaks_idx[0]] == pytest.approx(0.168299, rel=1e-5)
+
