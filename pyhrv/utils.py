7e7cbca818ad5610fea9c559fee791f4ad1a01b9
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2019-07-30 15:28:23 +0300

    utils: methods to standardize 1d array shapes

--- pyhrv/utils.py
+++ pyhrv/utils.py
@@ -1,8 +1,16 @@
 import importlib
 from typing import NamedTuple
 
+import numpy as np
+
 
 def import_function_by_name(func_name):
+    """
+    Imports and returns a function obbject given it's full name (packages,
+    modules and function name, e.g. foo.bar.baz).
+    :param func_name: Full name.
+    :return: Function object (callable).
+    """
     mod_name, func_name = func_name.rsplit('.', 1)
 
     if not mod_name:
@@ -14,6 +22,13 @@ def import_function_by_name(func_name):
 
 
 def sec_to_time(sec: float):
+    """
+    Converts a time duration in seconds to days, hours, minutes, seconds and
+    milliseconds.
+    :param sec: Time in seconds.
+    :return: An object with d, h, m, s and ms fields representing the above,
+    respectively.
+    """
     if sec < 0:
         raise ValueError('Invalid argument value')
     d = int(sec // (3600 * 24))
@@ -34,3 +49,37 @@ class __T(NamedTuple):
     def __repr__(self):
         return f'{"" if self.d == 0 else f"{self.d}+"}' \
             f'{self.h:02d}:{self.m:02d}:{self.s:02d}.{self.ms:03d}'
+
+
+def np_squeeze_check(a: np.ndarray) -> np.ndarray:
+    """
+    Converts a row/column (2d) to a 1d array. Does nothing if it's already 1d.
+    Raises an error if it's not a row/col.
+    :param a: An ndarray of shape (N,) or (N,1) or (N,1).
+    :return: The same data, flattend to (N,).
+    """
+    a = a.squeeze()
+    if a.ndim != 1:
+        raise ValueError("The given array is not 1d")
+
+    return a
+
+
+def standardize_rri_trr(rri, trr=None):
+    """
+    Converts rri intervals and their times to 1d arrays. Creates zero-based
+    times if not provided.
+    :param rri: RR intervals.
+    :param trr: RR intervals times.
+    :return: rri, trr tuple after standardization.
+    """
+    rri = np_squeeze_check(rri)
+
+    if trr is None:
+        trr = np.r_[0., np.cumsum(rri)[:-1]]
+    else:
+        trr = np_squeeze_check(trr)
+        if len(trr) != len(rri):
+            raise ValueError("Shape mismatch between rri and trr")
+
+    return rri, trr
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 import pyhrv.utils as utils
@@ -44,3 +45,65 @@ class TestImportFunctionByName(object):
     def test_(self):
         f = utils.import_function_by_name('pyhrv.utils.sec_to_time')
         assert str(f(97 * 3600 + 59 * 60 + 59.456)) == "4+01:59:59.456"
+
+
+class TestNpSqueezeCheck(object):
+    @staticmethod
+    def _check_data(expected, actual):
+        assert actual.ndim == 1
+        assert np.all(actual == expected.reshape(-1))
+
+    def test_1d(self):
+        a = np.array([1, 2, 3])
+        b = utils.np_squeeze_check(a)
+        self._check_data(a, b)
+
+    def test_col(self):
+        a = np.c_[1:10]
+        b = utils.np_squeeze_check(a)
+        self._check_data(a, b)
+
+    def test_row(self):
+        a = np.c_[1:10].T
+        b = utils.np_squeeze_check(a)
+        self._check_data(a, b)
+
+    def test_2d(self):
+        a = np.c_[1:10, 2:11]
+        with pytest.raises(ValueError) as ex_info:
+            utils.np_squeeze_check(a)
+
+    def test_nd(self):
+        a = np.random.randn(3, 4, 5, 6, 7, 9)
+        with pytest.raises(ValueError) as ex_info:
+            utils.np_squeeze_check(a)
+
+    def test_nd_sqeezable(self):
+        a = np.random.randn(1, 1, 1, 1, 100, 1, 1, 1, 1)
+        b = utils.np_squeeze_check(a)
+        self._check_data(a, b)
+
+
+class TestStandardize(object):
+    @staticmethod
+    def _check_data(rri, trr, rri_new, trr_new):
+        assert rri_new.ndim == 1
+        assert trr_new.ndim == 1
+        assert np.all(rri_new == rri.reshape(-1))
+        assert np.all(trr_new == trr.reshape(-1))
+        assert rri_new.shape == trr_new.shape
+
+    def test_squeeze_dims(self):
+        rri = np.random.randn(1, 100)
+        trr = np.linspace(1.2, 111.1, num=100)
+
+        rri_new, trr_new = utils.standardize_rri_trr(rri, trr)
+        self._check_data(rri, trr, rri_new, trr_new)
+
+    def test_create_trr(self):
+        rri = np.random.randn(1, 100)
+
+        rri_new, trr_new = utils.standardize_rri_trr(rri)
+
+        trr_expected = np.r_[0, np.cumsum(rri)[:-1]]
+        self._check_data(rri, trr_expected, rri_new, trr_new)
